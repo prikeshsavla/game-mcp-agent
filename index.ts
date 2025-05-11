d6949dd0bc0062066ec15f35721021db16dcd99f
Prikesh Savla <prikesh@cashee.com>
2025-05-11 12:18:29 +0530

    Comment Index.ts commands

--- index.ts
+++ index.ts
@@ -62,58 +62,58 @@ export {
 
 
 // await chainMethods(array);
-for (let i = 0; i < 1; i++) {
-  await chainMethods([
-    { description: "Move to ash forest", method: () => move(-1, 0) },
-    { description: "Gather", method: () => loopAction(80) },
-    { description: "Move to woodshop", method: () => move(-2, -3) },
-    { description: "Craft", method: async () => craft("ash_plank", Math.floor(((await getInventoryItem("ash_wood")).quantity) / 10)) },
-    { description: "Move to bank", method: () => move(4, 1) },
-    depositFromInventory('ash_plank'),
-    depositFromInventory('sap'),
-  ]);
-}
-function depositFromInventory(item) {
-  return {
-    description: "Deposit", method: async () => {
-      const quantity = ((await getInventoryItem(item)).quantity)
-      if (quantity) {
-        return bankDeposit(item, quantity)
-      }
-      return { message: `No items for ${item}` }
-    }
-  }
-}
+// for (let i = 0; i < 1; i++) {
+//   await chainMethods([
+//     { description: "Move to ash forest", method: () => move(-1, 0) },
+//     { description: "Gather", method: () => loopAction(80) },
+//     { description: "Move to woodshop", method: () => move(-2, -3) },
+//     { description: "Craft", method: async () => craft("ash_plank", Math.floor(((await getInventoryItem("ash_wood")).quantity) / 10)) },
+//     { description: "Move to bank", method: () => move(4, 1) },
+//     depositFromInventory('ash_plank'),
+//     depositFromInventory('sap'),
+//   ]);
+// }
+// function depositFromInventory(item) {
+//   return {
+//     description: "Deposit", method: async () => {
+//       const quantity = ((await getInventoryItem(item)).quantity)
+//       if (quantity) {
+//         return bankDeposit(item, quantity)
+//       }
+//       return { message: `No items for ${item}` }
+//     }
+//   }
+// }
 
-async function fightMonster(monster, product, weapon) {
-  const monster_location = (await getMaps({ content_type: 'monster', content_code: monster }))[0]
-  const actions = [
-    { description: "Rest", method: () => rest() },
-    { description: "Fight", method: () => fight() },
-    { description: "Fight", method: () => fight() },
-  ]
-  let array: any[] = []
-  for (let i = 0; i < 30; i++) {
-    array = [...array, ...actions];
-  }
+// async function fightMonster(monster, product, weapon) {
+//   const monster_location = (await getMaps({ content_type: 'monster', content_code: monster }))[0]
+//   const actions = [
+//     { description: "Rest", method: () => rest() },
+//     { description: "Fight", method: () => fight() },
+//     { description: "Fight", method: () => fight() },
+//   ]
+//   let array: any[] = []
+//   for (let i = 0; i < 30; i++) {
+//     array = [...array, ...actions];
+//   }
 
-  await chainMethods([
-    { description: "Unequp weapn", method: () => unequip('weapon') },
-    { description: "Unequp weapn", method: () => equip(weapon, 'weapon') },
-    { description: "Move to fight", method: () => move(monster_location.x, monster_location.y) },
-    ...array,
-    // { description: "Fight", method: () => fight() },
-    { description: "Move to bank", method: () => move(4, 1) },
-    depositFromInventory(product),
-    depositFromInventory('apple')
-  ])
+//   await chainMethods([
+//     { description: "Unequp weapn", method: () => unequip('weapon') },
+//     { description: "Unequp weapn", method: () => equip(weapon, 'weapon') },
+//     { description: "Move to fight", method: () => move(monster_location.x, monster_location.y) },
+//     ...array,
+//     // { description: "Fight", method: () => fight() },
+//     { description: "Move to bank", method: () => move(4, 1) },
+//     depositFromInventory(product),
+//     depositFromInventory('apple')
+//   ])
 
-}
+// }
 
-await fightMonster('red_slime', 'red_slimeball', 'water_bow')
-await fightMonster('green_slime', 'green_slimeball', 'sticky_dagger')
-await fightMonster('blue_slime', 'blue_slimeball', 'sticky_sword')
-await fightMonster('yellow_slime', 'yellow_slimeball', 'fire_staff')
+// await fightMonster('red_slime', 'red_slimeball', 'water_bow')
+// await fightMonster('green_slime', 'green_slimeball', 'sticky_dagger')
+// await fightMonster('blue_slime', 'blue_slimeball', 'sticky_sword')
+// await fightMonster('yellow_slime', 'yellow_slimeball', 'fire_staff')
 // await move(0, 1)
 // for (let i = 0; i < 16; i++) {
 // await chainMethods([
